cf8fcdb2dd5b9d35f35b12581a1f221ce5017db8
hiruocha <hiruocha@tuta.io>
2025-11-17 18:39:22 +0800

    chore: 添加清理 blob 脚本

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+find . -type f -name "*.webp" -delete
+find . -type f -name "*.jpg" -delete
+find . -type f -name "*.png" -delete
+find . -type f -name "*.mp3" -delete
+find . -type f -name "*.ttf" -delete
